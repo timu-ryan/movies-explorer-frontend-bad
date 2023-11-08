86fe17b05ea0ea6a67fda548df5fc5f5cf31d37d
Timur Gafarov <timu.ryan@yandex.ru>
2023-11-08 18:43:03 +0300

    created new branch level-2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-import Main from '../Main/Main'
+import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies'
-import Profile from '../Profile/Profile'
-import PageNotFound from '../PageNotFound/PageNotFound'
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 import Navigation from '../Navigation/Navigation';
 
